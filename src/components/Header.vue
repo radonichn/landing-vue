5dd2d5e1fea94a7b31a9d1df684461468bd5f01f
Nikolay Radonich <nikolayradonich@gmail.com>
2019-08-22 19:46:01 +0300

    created project; added packages (bs, scss); created header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-light bg-light d-flex justify-content-between">
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse show">
+      <div class="navbar-nav d-flex flex-row">
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Support</a>
+        <a class="nav-item nav-link" href="#">Blog</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  .navbar-nav {
+    flex-basis: 0;
+    flex-grow: 0;
+    .nav-item {
+    }
+  }
+}
+</style>
